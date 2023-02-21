f4c41a0d4eec250b893231fdf514985849463d74
Pixel <p1x9lll@gmail.com>
2023-02-21 23:34:30 +0200

    Added indents and ILog into MusicLibrary.cs

--- NoiseBox/MusicLibrary.cs
+++ NoiseBox/MusicLibrary.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NoiseBox.Log;
 
 namespace NoiseBox {
     public class Song {
@@ -26,14 +27,24 @@ namespace NoiseBox {
         private const string _jsonFilePath = "music_library.json";
         private List<Song> _songs = new List<Song>();
         private List<Playlist> _playlists = new List<Playlist>();
-        public MusicLibrary() {
+        private ILog _log;
+
+        public MusicLibrary(ILog log) {
+            if (log == null) {
+                throw new ArgumentNullException("Log can`t be null");
+            }
+
+            _log = log;
+
             LoadFromJson();
         }
+
         private void LoadFromJson() {
             if (File.Exists(_jsonFilePath)) {
                 string json = File.ReadAllText(_jsonFilePath);
                 var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto };
                 var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json, settings);
+
                 _songs = ((JArray)data["songs"]).ToObject<List<Song>>();
                 _playlists = ((JArray)data["playlists"]).ToObject<List<Playlist>>();
             }
@@ -47,6 +58,7 @@ namespace NoiseBox {
                 { "songs", JArray.FromObject(_songs) },
                 { "playlists", JArray.FromObject(_playlists) }
             };
+
             var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto };
             string json = JsonConvert.SerializeObject(data, Formatting.Indented, settings);
             File.WriteAllText(_jsonFilePath, json);
@@ -67,9 +79,11 @@ namespace NoiseBox {
 
         public void RemoveSong(string songId) {
             _songs.RemoveAll(s => s.Id == songId);
+
             foreach (var playlist in _playlists) {
                 playlist.SongIds.Remove(songId);
             }
+
             SaveToJson();
         }
 
@@ -82,11 +96,13 @@ namespace NoiseBox {
 
         public void RemovePlaylist(string playlistName) {
             _playlists.RemoveAll(p => p.Name == playlistName);
+
             SaveToJson();
         }
 
         public void AddSongToPlaylist(string songId, string playlistName) {
             var playlist = _playlists.Find(p => p.Name == playlistName);
+
             if (playlist != null) {
                 // Check if the song is already in the playlist
                 if (!playlist.SongIds.Contains(songId)) {
@@ -98,6 +114,7 @@ namespace NoiseBox {
 
         public void RemoveSongFromPlaylist(string songId, string playlistName) {
             var playlist = _playlists.Find(p => p.Name == playlistName);
+
             if (playlist != null) {
                 playlist.SongIds.Remove(songId);
                 SaveToJson();
@@ -107,6 +124,7 @@ namespace NoiseBox {
         public void MoveSongToPlaylist(string songId, string fromPlaylist, string toPlaylist) {
             var from = _playlists.Find(p => p.Name == fromPlaylist);
             var to = _playlists.Find(p => p.Name == toPlaylist);
+
             if (from != null && to != null) {
                 from.SongIds.Remove(songId);
                 to.SongIds.Add(songId);
@@ -123,6 +141,7 @@ namespace NoiseBox {
         public List<Song> GetSongsFromPlaylist(string playlistName) {
             var playlist = _playlists.Find(p => p.Name == playlistName);
             var songsFromPlaylist = new List<Song>();
+
             if (playlist != null) {
                 foreach (var songId in playlist.SongIds) {
                     songsFromPlaylist.Add(_songs.Find(p => p.Id == songId));
