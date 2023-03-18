aa27395f041b25716aeeed2cd99d6c6964330ec4
Yevhenii <atlantis.j11@gmail.com>
2023-03-18 20:53:39 +0200

    Added folder selection to DownloadsWindow and saving song to selected playlist or if playlist isn't selected to the first one.

--- NoiseBox UI/DownloadsWindow.xaml.cs
+++ NoiseBox UI/DownloadsWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -16,12 +18,31 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using static System.Net.WebRequestMethods;
+using WinForms = System.Windows.Forms;
+using NoiseBox;
 
 namespace NoiseBox_UI {
-    public partial class DownloadsWindow : Window {
+    public partial class DownloadsWindow : Window, INotifyPropertyChanged {
+
+        private Process process;
+
+        private string _selectedDirectory = Directory.GetCurrentDirectory();
+        public string SelectedDirectory { 
+            get {
+                return _selectedDirectory;
+            }
+            set {
+                _selectedDirectory = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public DownloadsWindow() {
             InitializeComponent();
             WinMax.DoSourceInitialized(this);
+            DataContext = this;
         }
 
         private void WindowSizeChanged(object sender, SizeChangedEventArgs e) {
@@ -52,8 +73,6 @@ namespace NoiseBox_UI {
             Cancel_Click(null, null);
         }
 
-        Process process;
-
         private async void Download_Click(object sender, RoutedEventArgs e) {
             yt_dlp_Output.Text = "Starting to download...";
             
@@ -71,18 +90,20 @@ namespace NoiseBox_UI {
             var directory = System.AppContext.BaseDirectory.Split(Path.DirectorySeparatorChar);
             var slice = new ArraySegment<string>(directory, 0, directory.Length - 4);
             var BinariesDirPath = Path.Combine(Path.Combine(slice.ToArray()), "Binaries");
+            var ffmpegLocation = Path.Combine(BinariesDirPath, @"ffmpeg\bin");
 
             var psi = new ProcessStartInfo(Path.Combine(BinariesDirPath, "yt-dlp.exe")) {
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 UseShellExecute = false,
                 CreateNoWindow = true,
-                Arguments = $" --ffmpeg-location \"{Path.Combine(BinariesDirPath, @"ffmpeg\bin")}\" -x --audio-format mp3 -o %(title)s.%(ext)s \"{LinkTextBox.Text}\""
+                Arguments = $" --ffmpeg-location \"{ffmpegLocation}\" -x --audio-format mp3 -o \"{SelectedDirectory}\\%(title)s.%(ext)s\" \"{LinkTextBox.Text}\""
             };
 
             process = new Process { StartInfo = psi };
 
             var isDownloading = false;
+            string downloadedFilePath = "";
 
             process.OutputDataReceived += (_, e) => {
                 if (!string.IsNullOrEmpty(e.Data)) {
@@ -110,6 +131,12 @@ namespace NoiseBox_UI {
 
                         if (e.Data.StartsWith("[ExtractAudio]")) {
                             yt_dlp_Output.Text = $"Extracting audio...";
+                            
+                            match = Regex.Match(e.Data, @"[A-Z]:.*\.mp3");
+
+                            if (match.Success) {
+                                downloadedFilePath = match.ToString();
+                            }
 
                             DownloadingProgress.Visibility = Visibility.Collapsed;
                             (Owner as MainWindow).FunctionButtons.DownloadingProgress.Visibility = Visibility.Collapsed;
@@ -143,6 +170,8 @@ namespace NoiseBox_UI {
 
             if (!hadErrors) {
                 yt_dlp_Output.Inlines.Add(new Run("[Downloading finished]") { Foreground = Brushes.LawnGreen });
+
+                AddSongToSelectedPlaylist(downloadedFilePath);
             }
             else {
                 yt_dlp_Output.Inlines.Add(new Run("[Error while downloading]") { Foreground = Brushes.IndianRed });
@@ -158,9 +187,11 @@ namespace NoiseBox_UI {
             }
         }
 
-        private void Cancel_Click(object sender, RoutedEventArgs e) {
-            process.Close();
-            process.Dispose();
+        private void Cancel_Click(object sender, RoutedEventArgs e) { // is also called when window is closed
+            if (process != null) {
+                process.Close();
+                process.Dispose();
+            }
 
             yt_dlp_Output.Text = "";
             yt_dlp_Output.Inlines.Add(new Run("[Canceled]") { Foreground = Brushes.IndianRed });
@@ -170,6 +201,43 @@ namespace NoiseBox_UI {
 
             DownloadColumn.Width = new GridLength(100, GridUnitType.Star);
             CancelColumn.Width = new GridLength(0, GridUnitType.Star);
+        }
+
+        private void SaveTo_Click(object sender, RoutedEventArgs e) {
+            var dialog = new WinForms.FolderBrowserDialog();
+            dialog.InitialDirectory = SelectedDirectory;
+            var res = dialog.ShowDialog();
+
+            if (res == WinForms.DialogResult.OK) {
+                SelectedDirectory = dialog.SelectedPath;
+            }
+        }
+
+        private void AddSongToSelectedPlaylist(string downloadedFilePath) {
+            Song song = new Song { Path = downloadedFilePath };
+
+            if (MusicLibrary.AddSong(song)) {
+                Playlist selectedPlaylist = (Owner as MainWindow).SelectedPlaylist;
+
+                if (selectedPlaylist == null) {
+                    selectedPlaylist = MusicLibrary.GetPlaylists().FirstOrDefault();
+
+                    if (selectedPlaylist != null) {
+                        (Owner as MainWindow).SelectPlaylistByName(selectedPlaylist.Name);
+
+                        MusicLibrary.AddSongToPlaylist(song.Id, selectedPlaylist.Name);
+                        (Owner as MainWindow).SongList.List.Items.Add(song);
+                    }
+                }
+                else {
+                    MusicLibrary.AddSongToPlaylist(song.Id, selectedPlaylist.Name);
+                    (Owner as MainWindow).SongList.List.Items.Add(song);
+                }
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
